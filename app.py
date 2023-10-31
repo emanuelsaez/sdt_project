2b7969c47d91d1af84b9cb02269b9f8900a370c6
emanuelsaez <148696500+emanuelsaez@users.noreply.github.com>
2023-10-31 22:45:54 +0000

    ok

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import plotly.express as px
 import altair
 
-cars = pd.read_csv('/workspaces/sdt_project/vehicles_us.csv')
+cars = pd.read_csv('/sdt_project/vehicles_us.csv')
 
 st.header('_Listing duration per condition_')
 
